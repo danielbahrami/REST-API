14f7c2fe4f8b0609fbaf0a366a3d2db5c9b4fe72
Daniel Bahrami <daniel@bahrami.dk>
2022-08-03 19:47:24 +0200

    Add hard-coded mockup data

--- api/main.go
+++ api/main.go
@@ -6,3 +6,11 @@ type person struct {
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 }
+
+var people = []person{
+	{ID: 1, FirstName: "Emalee", LastName: "Creigan", Email: "ecreigan0@nature.com"},
+	{ID: 2, FirstName: "Mariellen", LastName: "Peerless", Email: "mpeerless1@dot.gov"},
+	{ID: 3, FirstName: "Gabie", LastName: "Brims", Email: "gbrims2@cnbc.com"},
+	{ID: 4, FirstName: "Francesco", LastName: "Morman", Email: "fmorman3@rediff.com"},
+	{ID: 5, FirstName: "Ario", LastName: "Denerley", Email: "adenerley4@state.gov"},
+}
