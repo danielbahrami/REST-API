3b134038b6bf0a1f98b635407e28cd6be260bfa1
Daniel Bahrami <daniel@bahrami.dk>
2022-08-03 22:56:28 +0200

    Change id of type int to string

--- api/main.go
+++ api/main.go
@@ -6,18 +6,18 @@ import (
 )
 
 type person struct {
-	ID        int    `json:"id"`
+	ID        string `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 }
 
 var people = []person{
-	{ID: 1, FirstName: "Emalee", LastName: "Creigan", Email: "ecreigan0@nature.com"},
-	{ID: 2, FirstName: "Mariellen", LastName: "Peerless", Email: "mpeerless1@dot.gov"},
-	{ID: 3, FirstName: "Gabie", LastName: "Brims", Email: "gbrims2@cnbc.com"},
-	{ID: 4, FirstName: "Francesco", LastName: "Morman", Email: "fmorman3@rediff.com"},
-	{ID: 5, FirstName: "Ario", LastName: "Denerley", Email: "adenerley4@state.gov"},
+	{ID: "1", FirstName: "Emalee", LastName: "Creigan", Email: "ecreigan0@nature.com"},
+	{ID: "2", FirstName: "Mariellen", LastName: "Peerless", Email: "mpeerless1@dot.gov"},
+	{ID: "3", FirstName: "Gabie", LastName: "Brims", Email: "gbrims2@cnbc.com"},
+	{ID: "4", FirstName: "Francesco", LastName: "Morman", Email: "fmorman3@rediff.com"},
+	{ID: "5", FirstName: "Ario", LastName: "Denerley", Email: "adenerley4@state.gov"},
 }
 
 func getPeople(c *gin.Context) {
