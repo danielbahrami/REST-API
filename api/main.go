22fc9ab67f6b3d083bc87212f0336147373117d5
Daniel Bahrami <daniel@bahrami.dk>
2022-08-05 01:45:18 +0200

    Move user struct and data to separate file

--- api/main.go
+++ api/main.go
@@ -5,21 +5,6 @@ import (
 	"net/http"
 )
 
-type user struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-}
-
-var users = []user{
-	{ID: "1", FirstName: "Emalee", LastName: "Creigan", Email: "ecreigan0@nature.com"},
-	{ID: "2", FirstName: "Mariellen", LastName: "Peerless", Email: "mpeerless1@dot.gov"},
-	{ID: "3", FirstName: "Gabie", LastName: "Brims", Email: "gbrims2@cnbc.com"},
-	{ID: "4", FirstName: "Francesco", LastName: "Morman", Email: "fmorman3@rediff.com"},
-	{ID: "5", FirstName: "Ario", LastName: "Denerley", Email: "adenerley4@state.gov"},
-}
-
 func getUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
 }
