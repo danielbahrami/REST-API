1935f9e380265e99632ac77686f8ccb71dcd247a
Daniel Bahrami <daniel@bahrami.dk>
2022-08-04 02:36:52 +0200

    Change naming of "person/people" to "user/users"

--- api/main.go
+++ api/main.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-type person struct {
+type user struct {
 	ID        string `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 }
 
-var people = []person{
+var users = []user{
 	{ID: "1", FirstName: "Emalee", LastName: "Creigan", Email: "ecreigan0@nature.com"},
 	{ID: "2", FirstName: "Mariellen", LastName: "Peerless", Email: "mpeerless1@dot.gov"},
 	{ID: "3", FirstName: "Gabie", LastName: "Brims", Email: "gbrims2@cnbc.com"},
@@ -20,35 +20,35 @@ var people = []person{
 	{ID: "5", FirstName: "Ario", LastName: "Denerley", Email: "adenerley4@state.gov"},
 }
 
-func getPeople(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, people)
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
 }
 
-func getPersonById(c *gin.Context) {
+func getUserById(c *gin.Context) {
 	id := c.Param("id")
-	for _, person := range people {
-		if person.ID == id {
-			c.IndentedJSON(http.StatusOK, person)
+	for _, user := range users {
+		if user.ID == id {
+			c.IndentedJSON(http.StatusOK, user)
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "person not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
 
-func addPerson(c *gin.Context) {
-	var newPerson person
-	if err := c.BindJSON(&newPerson); err != nil {
+func addUser(c *gin.Context) {
+	var newUser user
+	if err := c.BindJSON(&newUser); err != nil {
 		return
 	}
-	people = append(people, newPerson)
-	c.IndentedJSON(http.StatusCreated, newPerson)
+	users = append(users, newUser)
+	c.IndentedJSON(http.StatusCreated, newUser)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/people", getPeople)
-	router.GET("/people/:id", getPersonById)
-	router.POST("/people", addPerson)
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getUserById)
+	router.POST("/users", addUser)
 	err := router.Run("localhost:8080")
 	if err != nil {
 		return
