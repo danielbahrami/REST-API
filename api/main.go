801a8d2c2c8ea84a621ddda09ff7cbec994e76e2
Daniel Bahrami <daniel@bahrami.dk>
2022-08-07 04:17:44 +0200

    Empty slice of type struct

--- api/main.go
+++ api/main.go
@@ -12,13 +12,7 @@ type user struct {
 	Email     string `json:"email"`
 }
 
-var users = []user{
-	{ID: "1", FirstName: "Emalee", LastName: "Creigan", Email: "ecreigan0@nature.com"},
-	{ID: "2", FirstName: "Mariellen", LastName: "Peerless", Email: "mpeerless1@dot.gov"},
-	{ID: "3", FirstName: "Gabie", LastName: "Brims", Email: "gbrims2@cnbc.com"},
-	{ID: "4", FirstName: "Francesco", LastName: "Morman", Email: "fmorman3@rediff.com"},
-	{ID: "5", FirstName: "Ario", LastName: "Denerley", Email: "adenerley4@state.gov"},
-}
+var users []user
 
 func getUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
