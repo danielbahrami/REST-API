9c5283bbe1374d6118c6532a51698fece92181e0
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2023-01-23 21:43:54 +0100

    Add imports

--- api/main.go
+++ api/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
+	"os"
 )
 
 type user struct {
