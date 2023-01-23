861aa20a09be00d1e007e935cda3a9d23e057331
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2023-01-23 21:44:04 +0100

    Update user struct

--- api/main.go
+++ api/main.go
@@ -11,11 +11,11 @@ import (
 	"os"
 )
 
-type user struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
+type User struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	FirstName string             `json:"first_name" bson:"first_name"`
+	LastName  string             `json:"last_name" bson:"last_name"`
+	Email     string             `json:"email" bson:"email"`
 }
 
 var users []user
